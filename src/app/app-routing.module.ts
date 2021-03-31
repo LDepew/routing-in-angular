c67d7e5ec9be7b52dc3cfa29f03a00c7961206a7
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-31 13:42:14 -0400

    add first routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-
-  { path: '**', component: ABC}
+  {path: 'home', component: HomeComponent }
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
