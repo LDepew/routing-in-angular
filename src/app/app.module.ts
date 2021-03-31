636552bbdd97e40579690393565a818bbcb9db3b
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-31 14:48:49 -0400

    create user page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,7 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, HttpClientModule
     AppRoutingModule
   ],
   providers: [],
